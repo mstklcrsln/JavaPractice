a991761573f6f44055a87208ae6e1696186d30f4
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-10 17:16:59 +0300

    last updates

---
+++ src/NovemberStudiesAndTasks/day22/day24_Tasks/Task10_RemoveElement.java
@@ -0,0 +1,32 @@
+package NovemberStudiesAndTasks.day22.day24_Tasks;
+
+import java.util.Arrays;
+
+public class Task10_RemoveElement {
+
+    public static void main(String[] args) {
+
+        int [] array = {10, 20, 30, 40, 50, 60};
+        int index= 3;
+        int [] removed = removeIntegerElement(array,index);
+        System.out.println(Arrays.toString(removed));
+    }
+        public static int [] removeIntegerElement (int [] arr, int index){
+        int [] removed = new int [arr.length-1];
+        int j=0;
+            for (int i = 0; i < arr.length; i++) {
+                if (i== index){
+                    continue;
+                }
+                removed[j++]= arr[i];
+            }
+            return removed;
+        }
+}
+/*
+10. create a method named removeElement that passes one integer array and one integer, the method removes the integer index from the integer array and returns the new array
+			Ex:
+				array = {10, 20, 30, 40, 50, 60}
+				index = 2
+				removeElement(array, index) ==== {10, 20, 40, 50, 60}
+ */
---
+++ src/NovemberStudiesAndTasks/day22/day24_Tasks/Task9_IsContained.java
@@ -0,0 +1,28 @@
+package NovemberStudiesAndTasks.day22.day24_Tasks;
+
+public class Task9_IsContained {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int num = 5;
+        boolean isContaine = isIntegerContained(arr,num);
+        System.out.println(isContaine);
+    }
+    public static boolean isIntegerContained(int[] arr, int num) {
+        boolean isContaine = false;
+        for (int eachElement : arr) {
+            if (eachElement == num) {
+                isContaine = true;
+            }
+        } return isContaine;
+    }
+}
+
+/*
+9. Create a method named contains that passes one integer array and one integer parameters,
+the method returns true if the given integer is contained in the given array, otherwise returns false
+									Ex:
+					arr = {1,2,3,4,5,6,7};
+					num = 10;
+					contains(arr, num) ===> false
+ */
