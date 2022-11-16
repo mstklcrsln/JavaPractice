88af5e69d37933d7b89336c9d753d09d08976a05
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-16 18:08:01 +0300

    last updates

---
+++ src/officeHours/Day3.java
@@ -0,0 +1,28 @@
+package officeHours;public class Day3 {
+    public static void main(String[] args) {
+
+      /*  int a= 25;
+
+        System.out.println(++a); //26
+        System.out.println(a);//26
+        System.out.println(a--);//26
+        System.out.println(a);//25
+
+        int f =100;
+        f++;
+        System.out.println(f); //101*/
+
+        System.out.println(true); // true
+        System.out.println(true && true); // true
+        System.out.println(true && false); // false
+        System.out.println(false && true); // false
+        System.out.println(false && false); // false
+        System.out.println("----------");
+        System.out.println(true); // true
+        System.out.println(true || true); // true
+        System.out.println(true || false); // true
+        System.out.println(false || true); // true
+        System.out.println(false || false); // false
+
+    }
+}
