47bc2eaff44100b07722373db361b3e3ece3fdcf
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-17 15:03:02 +0300

    last updates

--- src/wiseJavaCourse/ArrayListStudies/ArrayListIntro.java
+++ src/wiseJavaCourse/ArrayListStudies/ArrayListIntro.java
@@ -6,11 +6,24 @@ import java.util.List;
 public class ArrayListIntro {
     public static void main(String[] args) {
 
-
         List<Integer> numbers = new ArrayList<>();
 
         numbers.add(3);
         numbers.add(4);
         System.out.println(numbers); //[3, 4]
+
+        System.out.println("--------------");
+        // add all vowel (sesli harf) to the Arraylist
+
+        List<String> vowels= new ArrayList<>();
+        // we write all elements in an array and than
+        // put them into a Arraylist by using for loop
+
+        String [] arr = {"ı","o","u","i","e","a"};
+
+        for (int i = 0; i < arr.length; i++) {
+            vowels.add(arr[i]);
+        }
+        System.out.println(vowels); //[ı, o, u, i, e, a]
     }
 }
