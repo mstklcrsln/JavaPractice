f68c6ebb5a6a3feb5e7cac191a8c126ba3ed7903
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-17 12:56:24 +0300

    last updates

---
+++ src/bulutluoz/FindEvenNumber.java
@@ -0,0 +1,19 @@
+package bulutluoz;
+
+public class FindEvenNumber {
+    public static void main(String[] args) {
+
+        // how many even numbers in the multi dimensional array
+        int [][] arr= {{2,3,5,6,4,7}, {1,2,5,6,9,87,4}, {2,5,6,9,8,7,4,33,66}};
+
+        int pieceOfEvenNumbers =0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j]%2==0){
+                    pieceOfEvenNumbers++;
+                }
+            }
+            System.out.println("Piece of even numbers of arrays: "+ pieceOfEvenNumbers); //Piece of even numbers of arrays: 11
+        }
+    }
+}
