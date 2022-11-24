d82c87ab4368827e3f6cd975598cb8a8e455666f
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-24 17:09:41 +0300

    day28_Tasks

---
+++ src/novemberStudiesAndTasks/day28_Tasks/Task6.java
@@ -0,0 +1,28 @@
+package novemberStudiesAndTasks.day28_Tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(10,2,3,4,5,6,98,7,444,55,66));
+
+       int min= list.get(0);
+        for (Integer integer : list) {
+            if (integer<min){
+                min=integer;
+            }
+        }
+        System.out.println("min = " + min); //min = 2
+    }
+}
+/*
+6. Write a program that can find the minimum number from an ArrayList of integers
+		Ex:
+			list = [1,2,3,4,5];
+			output:
+				1
+ */
---
+++ src/novemberStudiesAndTasks/day28_Tasks/Task7.java
@@ -0,0 +1,34 @@
+package novemberStudiesAndTasks.day28_Tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task7 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,2,3,4,4,5,6,7,7));
+
+       int firstDubl= 0;
+        for (Integer each : list) {
+            int freq=0;
+            for (Integer element : list){
+                if (each== element){
+                    freq++;
+                }
+            }
+            if (freq>1){
+                firstDubl=each;
+                break;
+            }
+        }
+        System.out.println("firstDubl = " + firstDubl); //firstDubl = 2
+    }
+}
+/*
+7. Write a program that can return the first duplicated element from an arrayList of Integer
+		Ex:
+			list = [1,2,2,3,4,4,5,6,7,7];
+			output:
+				2
+ */
