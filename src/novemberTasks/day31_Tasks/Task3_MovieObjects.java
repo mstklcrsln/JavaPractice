78e025c2ceb801b261c5d13c4198f791592cbdb9
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-25 11:23:07 +0300

    day31_Tasks

---
+++ src/novemberTasks/day31_Tasks/Task3_MovieObjects.java
@@ -0,0 +1,19 @@
+package novemberTasks.day31_Tasks;
+
+public class Task3_MovieObjects {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+3.2create a class called MovieObjects
+            1. create an object of the movie:
+                    title: Journey to SDET: Cydeo Batch 25
+                    country: USA
+                    Genre: Adventure, Comedy, Thriller
+                    release date: 10/25/2021
+                    director: Kuzzat Altay
+                    Casts: Asiya, Adam, Muhtar and 5 more students from your group
+
+        print the full info of the movie
+ */
--- src/novemberTasks/day31_Tasks/Task3_MovieTask.java
+++ src/novemberTasks/day31_Tasks/Task3_MovieTask.java
@@ -1,15 +1,38 @@
 package novemberTasks.day31_Tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Task3_MovieTask {
-    public static void main(String[] args) {
 
+    public String country, title, genre, releaseDate, director;
+    public ArrayList<String> casts = new ArrayList<>();
 
-
+    public Task3_MovieTask(String country, String title, String releaseDate, String director) {
+        this.country = country;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.director = director;
+    }
+    public void addCast (String name){
+        casts.add(name);
+    }
+    public void addCasts (String [] names){
+        casts.addAll(Arrays.asList(names));
+    }
+    public String toString() {
+        return "Task3_MovieTask{" +
+                "country='" + country + '\'' +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", total number of casts=" + casts.size() +
+                '}';
     }
 }
 /*
 3. Movie Task:
-
 	3.1. Create a class named Movie
 	    Attributes:
 	        country (String), title (String), Genre (String), releaseDate (String), director (String), casts (ArrayList<String>)
@@ -20,15 +43,4 @@ public class Task3_MovieTask {
 	        addCast(String): adds the given string argument to the arrayList casts
 	        addCasts(String[]): adds the given string array argument to the arrayList casts
 	        toString(): returns the name of country, title, release date, and total number of casts
-
-    3.2create a class called MovieObjects
-            1. create an object of the movie:
-                    title: Journey to SDET: Cydeo Batch 25
-                    country: USA
-                    Genre: Adventure, Comedy, Thriller
-                    release date: 10/25/2021
-                    director: Kuzzat Altay
-                    Casts: Asiya, Adam, Muhtar and 5 more students from your group
-
-        print the full info of the movie
  */
