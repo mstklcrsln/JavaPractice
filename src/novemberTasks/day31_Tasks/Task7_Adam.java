73fe2b732d9c7d7c87f69826a6afd771e40d4634
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-25 13:37:27 +0300

    day31_Tasks

---
+++ src/novemberTasks/day31_Tasks/Task7_Adam.java
@@ -0,0 +1,18 @@
+package novemberTasks.day31_Tasks;
+
+public class Task7_Adam {
+
+
+}
+/*
+Task-1
+The class HouseItems holds a few attributes and has a method called toString. The attributes are name and color -both are strings and amount which is an int. toString returns a string showing all the Atts in a format.
+for example:
+HouseItems a = new HouseItems();
+a.name = "table";
+a.color = "brown";
+a.amount = 1;
+System.out.println(a.toString());
+name: table color: brown amount: 1
+Create the required attributes and method inside the HouseItems class
+ */
