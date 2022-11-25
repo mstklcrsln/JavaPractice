58f4e8f197372cf67f2cfcab3e6ef3f481846597
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-25 14:39:43 +0300

    Day 33 Tasks

--- src/novemberTasks/day33_Tasks/Car.java
+++ src/novemberTasks/day33_Tasks/Car.java
@@ -1,6 +1,36 @@
 package novemberTasks.day33_Tasks;
 
 public class Car {
+
+    public static int numberOfWheels= 4;
+    public static boolean hasBattery = false;
+    public static boolean hasSeats =true;
+
+    public String make;
+    public String model;
+    public String color;
+    public int year;
+    public double price;
+
+    public Car(String make, String model, String color, int year, double price) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+    }
+    public void drive(){
+        System.out.println("you can drive this car");
+    }
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
 }
 /*
 7. Car Task:
@@ -8,14 +38,8 @@ public class Car {
             Attributes:
                  instance: make, model, color, year, price
                  static: numberOfWheels, hasBattery, hasSeats
-
             Add a constructor to initialize all the fields
-
            Methods:
                 drive()
                 toString()
-
-
-
-
  */
